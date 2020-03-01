502326415fbc5bdf1fc2e839de5f35bf856aad83
Kwang Myung, Jeon <lordet01@gmail.com>
2020-03-01 12:57:35 +0900

    update

--- scoring_track3.py
+++ scoring_track3.py
@@ -2,11 +2,11 @@ import os
 import json
 import math
 
-#REF_JSON = 'GC19_t3/lab_GC19_pseudotrue.json' 
-#EST_JSON = 'GC19_t3/t3_kict_v2_output.json'
+REF_JSON = 'GC19_t3/lab_GC19_pseudotrue.json' 
+EST_JSON = 'GC19_t3/GC19t3_intflow2020_sim1_10.json'
 
-REF_JSON = 'GC20_KICT_t3/500_sel_results_t.json' 
-EST_JSON = 'GC20_KICT_t3/500_sel_results_t_GC20_sim1.json'
+#REF_JSON = 'GC20_KICT_t3/500_sel_results_t.json' 
+#EST_JSON = 'GC20_KICT_t3/500_sel_results_t_GC20_sim1_10.json'
 PI = math.pi
 
 with open(REF_JSON) as json_file:
@@ -21,11 +21,13 @@ for clip_ref in json_data_ref['track3_results']:
     clip_est = json_data_est['track3_results'][i]
     #print(clip_ref['id'])
 
-    if clip_ref['angle'] * clip_est['angle'] >= 0:
+    if (clip_ref['angle'] >= 0 and clip_est['angle'] >= 0) or \
+       (clip_ref['angle'] < 0 and clip_est['angle'] < 0):
         score = pow((clip_ref['angle'] - clip_est['angle'])/180.0*PI,2)
     else:
         score = pow(PI,2)
 
+    print(score)
     score_final += score
 
     i+=1
